680ea35495be90fc4ff41d7585e25c6f888919ab
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2024-05-02 23:55:48 -0500

    adding thigns to git

---
+++ Chronicle.Tests/UnitTests/MapperProfiles/MapperTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Chronicle.Tests.UnitTests.MapperProfiles
+{
+    public class MapperTests<TMapperProfile>
+        where TMapperProfile : Profile, new()
+    {
+        protected MapperConfiguration _config;
+        protected IMapper mapper;
+
+        public MapperTests()
+        {
+            _config = new MapperConfiguration(cfg => cfg.AddProfile<TMapperProfile>());
+            mapper = new Mapper(_config);
+        }
+
+        [Fact]
+        public void HasCorrectConfig()
+        {
+            _config.AssertConfigurationIsValid();
+        }
+
+        public static IMapper CreateMapper()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<TMapperProfile>());
+            return new Mapper(config);
+        }
+    }
+}
---
+++ Chronicle.Tests/UnitTests/Services/ServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using Chronicle.Domain.Repositories.Interfaces;
+using NSubstitute;
+
+namespace Chronicle.Tests.UnitTests.Services
+{
+    public class ServiceTests<TService>
+    {
+        protected readonly IAuthorRepository _authorRepo = Substitute.For<IAuthorRepository>();
+    }
+}
