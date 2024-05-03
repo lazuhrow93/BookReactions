271472c181fa3cc7d7b538328470e28fa8dcec4b
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2024-05-02 23:35:16 -0500

    adding tracking foor unit tests

---
+++ Chronicle.Tests/UnitTests/Services/AuthorServiceTests.cs
@@ -0,0 +1,28 @@
+﻿
+using Chronicle.Domain.Repositories.Interfaces;
+using NSubstitute;
+using Xunit;
+
+namespace Chronicle.Tests.UnitTests.Services
+{
+    public class AuthorServiceTests
+    {
+        public AuthorServiceTests()
+        {
+            
+        }
+
+        [Fact]
+        public void Add_Should_ReturnErrorOnExists()
+        {
+
+        } 
+    }
+
+    public class ServiceTests<TService>
+    {
+        protected readonly IAuthorRepository _authorRepo = Substitute.For<IAuthorRepository>();
+        protected readonly 
+
+    }
+}
