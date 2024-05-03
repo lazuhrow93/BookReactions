8ba57e7dc15844fbe718af596dc5fa1e3989ba96
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2024-05-02 23:53:01 -0500

    adding tests and test utilities

---
+++ Chronicle.Tests.Utility/Extensions/RepositoryResultExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Chronicle.Domain.Repositories;
+using FluentAssertions;
+
+namespace Chronicle.Tests.Utility.Extensions
+{
+    public static class RepositoryResultExtensions
+    {
+        public static RepositoryResult<TEntity> AssertEntityFound<TEntity>(this RepositoryResult<TEntity> result)
+        {
+            result.EntityFound.Should().BeTrue();
+            return result;
+        }
+    }
+}
--- Chronicle.Tests/UnitTests/Services/AuthorServiceTests.cs
+++ Chronicle.Tests/UnitTests/Services/AuthorServiceTests.cs
@@ -1,28 +1,46 @@
-﻿
-using Chronicle.Domain.Repositories.Interfaces;
-using NSubstitute;
+﻿using AutoMapper;
+using Chronicle.Tests.UnitTests.MapperProfiles;
 using Xunit;
+using Chronical.App.Mappers;
+using Chronical.App.Services.Interfaces;
+using Chronical.App.Services.Implementations;
+using Chronical.App.Helper;
+using Chronical.App.Services.Extensions;
+using Chronical.App.Models.IncomingDto;
+using NSubstitute;
+using Chronicle.Tests.Utility.Extensions;
 
 namespace Chronicle.Tests.UnitTests.Services
 {
-    public class AuthorServiceTests
+    public class AuthorServiceTests : ServiceTests<AuthorService>
     {
+        private readonly IMapper _mapper = MapperTests<DtoProfile>.CreateMapper();
+        private readonly IAuthorService _service;
+
         public AuthorServiceTests()
         {
-            
+            _service = new AuthorService(_authorRepo, _mapper);    
         }
 
         [Fact]
         public void Add_Should_ReturnErrorOnExists()
         {
+            var authorEntity = new AuthorFaker().Generate();
+            
+            _authorRepo
+                .GetByFullName(authorEntity.FirstName!, authorEntity.MiddleName, authorEntity.LastName!)
+                .Returns(authorEntity);
+
+            var result = _service.AddAuthor(new AuthorDto()
+            {
+                FirstName = authorEntity.FirstName,
+                MiddleName = authorEntity.MiddleName,
+                LastName = authorEntity.LastName,
+            });
+
+            result
+                .AssertEntityFound();
 
         } 
-    }
-
-    public class ServiceTests<TService>
-    {
-        protected readonly IAuthorRepository _authorRepo = Substitute.For<IAuthorRepository>();
-        protected readonly 
-
     }
 }
